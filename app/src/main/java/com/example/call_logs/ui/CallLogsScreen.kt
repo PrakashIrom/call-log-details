43b4ce39c9d24522aec8602cdecdc2464e68adce
prakash <apuiirom@gmailcom>
2024-10-18 21:21:17 +0530

    Search Bar Added

--- app/src/main/java/com/example/call_logs/ui/CallLogsScreen.kt
+++ app/src/main/java/com/example/call_logs/ui/CallLogsScreen.kt
@@ -6,10 +6,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -21,26 +29,47 @@ import com.example.call_logs.ui.theme.CalllogsTheme
 @Composable
 fun CallLogScreen(){
 
-    val items = listOf(CallLog("Incoming", "2:00", "12/12/2023", "1234567890"),
-        CallLog("Outgoing", "3:40", "12/12/2024", "1234567890"),
-        CallLog("Missed Call", "12:21", "12/12/2024", "1234567890"),
-        CallLog("Incoming", "9:30", "12/12/2023", "1234567890")
+    var search by remember{
+        mutableStateOf("")
+    }
+
+    val items = listOf(CallLog("Incoming", "2:00", "12/12/2023", "7645"),
+        CallLog("Outgoing", "3:40", "12/12/2024", "1119"),
+        CallLog("Missed Call", "12:21", "12/12/2024", "9999"),
+        CallLog("Incoming", "9:30", "12/12/2023", "6023")
         )
 
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(top=40.dp)){
-        items(items){item->
-            Card(modifier = Modifier.padding(10.dp).fillMaxWidth(),
-                colors = when(item.mobileNumber){
-                    "Incoming" -> CardDefaults.cardColors(Color.DarkGray)
-                    "Outgoing" -> CardDefaults.cardColors(Color.Green)
-                    else -> CardDefaults.cardColors(Color.Red)
-                }
-            ){
-                Column(modifier = Modifier.padding(10.dp)){
-                    Text(text = "Call duration: ${item.callType}", fontWeight = FontWeight.Bold)
-                    Text(text = "Date: ${item.callDuration}", fontWeight = FontWeight.Bold)
-                    Text(text = "Number: ${item.callDate}", fontWeight = FontWeight.Bold)
-                    Text(text = "Call type: ${item.mobileNumber}", fontWeight = FontWeight.Bold)
+    Column {
+
+        OutlinedTextField(
+            value = search,
+            onValueChange = { search = it },
+            label = { Text("Search") },
+            trailingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+            }, modifier = Modifier.padding(top = 40.dp, start = 10.dp, end = 10.dp, bottom = 18.dp).fillMaxWidth()
+        )
+
+        val searchItems = items.filter{
+            it.callDate.contains(search)
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(searchItems) { item ->
+                Card(
+                    modifier = Modifier.padding(10.dp).fillMaxWidth(),
+                    colors = when (item.mobileNumber) {
+                        "Incoming" -> CardDefaults.cardColors(Color.DarkGray)
+                        "Outgoing" -> CardDefaults.cardColors(Color.Green)
+                        else -> CardDefaults.cardColors(Color.Red)
+                    }
+                ) {
+                    Column(modifier = Modifier.padding(10.dp)) {
+                        Text(text = "Call duration: ${item.callType}", fontWeight = FontWeight.Bold)
+                        Text(text = "Date: ${item.callDuration}", fontWeight = FontWeight.Bold)
+                        Text(text = "Number: ${item.callDate}", fontWeight = FontWeight.Bold)
+                        Text(text = "Call type: ${item.mobileNumber}", fontWeight = FontWeight.Bold)
+                    }
                 }
             }
         }
